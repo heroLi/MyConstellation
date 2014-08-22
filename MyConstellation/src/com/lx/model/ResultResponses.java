55a943bb31a6a25a1165880797e58e10f14b565e
heroLi <lixiao_1991@sina.com>
2014-08-22 17:40:00 +0800

    json

---
+++ MyConstellation/src/com/lx/model/ResultResponses.java
@@ -0,0 +1,26 @@
+package com.lx.model;
+
+public class ResultResponses {
+
+	private String title;
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getRank() {
+		return rank;
+	}
+	public void setRank(String rank) {
+		this.rank = rank;
+	}
+	public String getValues() {
+		return values;
+	}
+	public void setValues(String values) {
+		this.values = values;
+	}
+	private String rank;
+	private String values;
+}
