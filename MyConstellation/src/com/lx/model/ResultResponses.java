9301fb81e35126611b412276692dc9afbbd8a44f
heroLi <lixiao_1991@sina.com>
2014-08-25 09:29:03 +0800

    结果类

---
+++ MyConstellation/src/com/lx/model/ResultResponses.java
@@ -0,0 +1,26 @@
+package com.lx.model;
+
+public class ResultResponses {
+
+	private String title;
+	private String rank;
+	private String value;
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getRank() {
+		return rank;
+	}
+	public void setRank(String rank) {
+		this.rank = rank;
+	}
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
+}
